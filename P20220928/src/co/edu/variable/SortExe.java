f93d228e22f93f0e98e7d56ed9006d2cb7bee3fb
sehun <tpgns5890@naver.com>
2022-09-28 23:11:07 +0900

    0928 (home)

--- P20220928/src/co/edu/variable/SortExe.java
+++ P20220928/src/co/edu/variable/SortExe.java
@@ -2,19 +2,30 @@ package co.edu.variable;
 
 public class SortExe {
 	public static void main(String[] args) {
-		int[] numAry = { 34, 21, 12 };
+		int[] numAry = { 34, 21, 64 };
 
 //		반복적인 처리.
 //		결과 : numAry = {12,21,34}
 //		케이스 : n1 > n2 > n3 => 
 //		케이스 : n1 < n2 > n3 =>
 
-		if (numAry[0] > numAry[1]) {	// 34>21
-			int temp = numAry[0];	
-			numAry[0] = numAry[1];	// 21
+		if (numAry[0] > numAry[1]) { // 34>21
+			int temp = numAry[0];
+			numAry[0] = numAry[1]; // 21
 			numAry[1] = temp;
 		}
-		for(int n : numAry) {
+		if (numAry[0] > numAry[2]) {
+			int temp = numAry[0];
+			numAry[0] = numAry[2];
+			numAry[2] = temp;
+		}
+
+		if (numAry[1] > numAry[2]) {
+			int temp = numAry[1];
+			numAry[1] = numAry[2];
+			numAry[2] = temp;
+		}
+		for (int n : numAry) {
 			System.out.println(n);
 		}
 	}
